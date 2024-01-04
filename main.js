f58f3ba34cd2896fdba3d45f21231091a5c844cc
jhynid8 <140052396+jhynid8@users.noreply.github.com>
2024-01-04 15:07:34 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,59 @@
-const toggleBtn  = document.querySelector('.navbar_toggleBtn');
-const menu  = document.querySelector('.navbar_menu');
+let adcoreDbElement = document.getElementById("adcore_db");
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
+window.addEventListener("scroll", function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 1000 && value <= 1530) {
+        adcoreDbElement.style.opacity = 1;
+        adcoreDbElement.style.transform = "translateX(0)";
+    } else {
+        adcoreDbElement.style.opacity = 0;
+        adcoreDbElement.style.transform = "translateX(-400px)";
+    }
+});
+
+let offerTextElement = document.getElementsByClassName("offer_text")[0];
+
+window.addEventListener("scroll", function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 1000 && value <= 1530) {
+        offerTextElement.style.opacity = 1;
+        offerTextElement.style.transform = "translateX(0)";
+    } else {
+        offerTextElement.style.opacity = 0;
+        offerTextElement.style.transform = "translateX(400px)";
+    }
+});
+
+let datamktElement = document.getElementById("data_mkt");
+
+window.addEventListener("scroll", function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 1530 && value <= 2030) {
+        datamktElement.style.opacity = 1;
+        datamktElement.style.transform = "translateX(0)";
+    } else {
+        datamktElement.style.opacity = 0;
+        datamktElement.style.transform = "translateX(-400px)";
+    }
+});
+
+let sec_offerTextElement = document.getElementsByClassName("offers2_text")[0];
+
+window.addEventListener("scroll", function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 1530 && value <= 2030) {
+        sec_offerTextElement.style.opacity = 1;
+        sec_offerTextElement.style.transform = "translateX(0)";
+    } else {
+        sec_offerTextElement.style.opacity = 0;
+        sec_offerTextElement.style.transform = "translateX(400px)";
+    }
 });
